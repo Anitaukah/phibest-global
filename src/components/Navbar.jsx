17904ea51d1ab6abb3aef03fe93c11e45bbb38d2
Anita <anitaukah2@gmail.com>
2025-05-01 11:05:17 +0100

    created the Navheader and Navbar component and styled them using tailwindcss

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+import Logo from "../assets/logo.png";
+
+const Navbar = () => {
+	return (
+		<nav className="bg-white">
+			<div className="flex justify-between items-center m-auto w-[1200px] min-w-64">
+				<img className="w-28 h-20" src={Logo} alt="Phibest global company logo"/>
+				<div className="text-[18px]">
+					<a href="#Home">Home</a>
+					<a href="#Service" className="ml-20">Services</a>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ src/components/Navheader.jsx
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+
+const Navheader = () => {
+	return (
+		<div className="bg-[#0000FA] ">
+			<div className="m-auto w-[1200px] min-w-64">
+				<ul className="flex justify-between text-white py-4 ">
+					<li>+234 (0) 803 677 6697</li>
+					<li>phizek4life@gmail.com</li>
+					<li>phizek4life@yahoo.com</li>
+					<li>KM.3 Harbour Road Davo Plaza, By Njinna Hotel</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Navheader;
