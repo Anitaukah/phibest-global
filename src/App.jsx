0205e3543e3e1b08fa0e8c2d43019f8161300fa1
Divine Orji <dpkreativ@gmail.com>
2025-05-08 22:16:27 +0100

    feature: add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@
 import "./App.css";
 import Navheader from "./components/Navheader";
 import Navbar from "./components/Navbar";
-import Hello from "./components/Hero";
+import Hero from "./components/Hero";
 import MetricsSection from "./components/MetricsSection";
 import About from "./components/About";
 import Policy from "./components/Policy";
 import Services from "./components/Services";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 function App() {
-	return (
-		<div>
-			<Navheader />
-			<Navbar />
-			<Hello />
-			<MetricsSection />
-			<About />
-			<Policy />
-			<Services />
-			<Footer />
-		</div>
-	);
+  return (
+    <div>
+      <Navheader />
+      <Navbar />
+      <Hero />
+      <MetricsSection />
+      <About />
+      <Policy />
+      <Services />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,29 @@
 /** @format */
 
 import React from "react";
+import bg from "../assets/Images/image-2.png";
 
 const Hero = () => {
-	return <div></div>;
+  return (
+    <section className="relative overflow-hidden text-white bg-black/35">
+      <img
+        src={bg}
+        alt="bg"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-10"
+      />
+      <div className="h-[80vh] flex flex-col justify-center items-center gap-10 text-center w-full max-w-6xl mx-auto">
+        <h1 className="text-7xl">Bitumen Cleaning and Oil-Based Mud (OBM).</h1>
+        <h3 className="text-5xl text-[#b0b0fd]">Quality you can trust</h3>
+        <p className="text-4xl">
+          We deliver Solutions that meets your Expectation
+        </p>
+
+        <button className="bg-[#eb1e23] text-white p-5 px-20 text-4xl rounded-xl w-max">
+          Our Services
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
